e5fe9f85d12dab477683260ac5842a989cac637c
Abdul-Baset-Codingal <abdulbaset.codingal@gmail.com>
2025-07-18 21:12:08 +0600

    deployment done

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,10 @@
 // authRoutes.js
 import express from "express";
-import { signup, login } from "../controllers/authController.js";
+import { signup, login, logout } from "../controllers/authController.js";
 
 const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/logout", logout);
+
 export default router;
