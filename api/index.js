72ba2d7c0ef21ea50003335deda4b9e62e3b59b3
Abdul-Baset-Codingal <abdulbaset.codingal@gmail.com>
2025-07-19 07:08:03 +0600

    nooo

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import serverless from "serverless-http";
 import app from "./app.js";
+import connectToDatabase from "./db.js";
+
+const mongoUri = process.env.MONGODB_URI;
+
+let isDbConnected = false;
+
+async function init() {
+    if (!isDbConnected) {
+        await connectToDatabase(mongoUri);
+        isDbConnected = true;
+    }
+}
+
+await init();
 
 export const handler = serverless(app);
--- api/server.js
+++ api/server.js
@@ -1,8 +1,14 @@
 // server.js
 import app from "./app.js";
+import connectToDatabase from "./db.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+async function startServer() {
+    await connectToDatabase(process.env.MONGODB_URI);
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
+
+startServer();
